55e10c58e1502e73d7cbd8a3f77a5e39f2e7121e
mgonpor <miguelgp2005@gmail.com>
2026-01-12 12:46:42 +0100

    trasteo

---
+++ daw-task/src/main/java/com/daw/web/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.daw.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors()
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET, "/tareas").permitAll()
+                .requestMatchers(HttpMethod.GET, "/tareas/*").authenticated()
+                .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+}
